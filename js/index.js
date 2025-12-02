4be9d76ecd490739eb692cb251a098ba5957118f
krjmrok <kriku2001@gmail.com>
2025-12-02 21:32:59 +0900

    mana本からさっそくColorPicker落としてみた

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 window.alert("Hello World!");
+const text = document.querySelector("#colorText");
+const color = document.querySelector("#colorPicker");
+
+const colorBg = () => {
+    text.textContent = `ColorCode：${color.value}`;
+}
+
+color.addEventListener("input",colorBg);
