aa217003780463013b8f2bd31c0b49d324cf0a80
krjmrok <kriku2001@gmail.com>
2025-12-09 21:18:09 +0900

    DOM操作をちょっとだけ入れてみた。

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 window.alert("Hello World!");
-const text = document.querySelector("#colorText");
+
+const text = document.querySelector("#colorText")
 const color = document.querySelector("#colorPicker");
+/*const search = document.querySelector("#searchText");👈試しにやってみたけど微妙...*/
 
 const colorBg = () => {
+    text.style.color = color.value;
     text.textContent = `ColorCode：${color.value}`;
 }
 
